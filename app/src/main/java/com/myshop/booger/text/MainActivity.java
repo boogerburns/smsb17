4e8eee31a42d077f0e378c247a6d4026b1c6d9cd
Boogerburns <boogerburns@gmail.com>
2018-10-17 08:41:40 -0400

    squashed some bugs, cleaned up some rough sections

--- app/src/main/java/com/myshop/booger/text/MainActivity.java
+++ app/src/main/java/com/myshop/booger/text/MainActivity.java
@@ -1,19 +1,20 @@
 package com.myshop.booger.text;
 
 import android.Manifest;
-import android.nfc.Tag;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.support.design.widget.FloatingActionButton;
+import android.telephony.SmsManager;
+import android.text.TextUtils;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.telephony.gsm.SmsManager;
+import android.view.View;
 import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,35 +28,27 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floatingActionButton);
-        final TextView phonenumber = (TextView) findViewById(R.id.phonenumber);
-        final TextView message = (TextView) findViewById(R.id.message);
-        final TextView textnumber = (TextView) findViewById(R.id.textnumber);
-
-        phonenumber.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                phonenumber.setText("");
-            }
-        });
-
-        message.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                message.setText("");
-            }
-        });
-
-        textnumber.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                textnumber.setText("");
-            }
-        });
+        final TextView phonenumber = getPhoneTextView();
+        final TextView message = getBodyTextView();
+        final TextView textnumber = getTextNumberTextView();
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String phoneNumber = phonenumber.getText().toString();
-                String messageString = message.getText().toString();
-                int numberoftexts = Integer.parseInt(textnumber.getText().toString());
-                sendSMS(phoneNumber,messageString, numberoftexts);
+                if (checkIfReadyToSend(phonenumber.getText().toString(),message.getText().toString(),textnumber.getText().toString())) {
+                    try {
+                        sendSMS(phonenumber.getText().toString(), message.getText().toString(), Integer.parseInt(textnumber.getText().toString()));
+                    } catch (Exception e) {
+                        System.out.println("Error :" + e.getMessage());
+                    }
+                }
+                else{
+                    Log.i("checkIfReadyToSend","Not ready to send text");
+                    Snackbar messagewindow = Snackbar.make(findViewById(R.id.mainCoordinatorLayout), "", 2);
+                    messagewindow.setText("Not ready to send texts check your inputs");
+                    messagewindow.setDuration(Snackbar.LENGTH_LONG);
+                    messagewindow.show();
+                }
             }
         });
 
@@ -78,11 +71,43 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            Log.i("action_settings","Settings on click");
+            Snackbar messagewindow = Snackbar.make(findViewById(R.id.mainCoordinatorLayout), "", 2);
+            messagewindow.setText("Settings has not been implemented yet");
+            messagewindow.setDuration(Snackbar.LENGTH_LONG);
+            messagewindow.show();
+        }
+
+        if (id == R.id.action_clear_all){
+            clear_all();
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void clear_all(){
+        TextView phoneView=getPhoneTextView();
+        TextView bodyView=getBodyTextView();
+        TextView textNumberView=getTextNumberTextView();
+
+        phoneView.setText(null);
+        bodyView.setText(null);
+        textNumberView.setText(null);
+
+    }
+
+    private TextView getPhoneTextView(){
+       return (TextView) findViewById(R.id.phonenumber);
+    }
+
+    private TextView getBodyTextView(){
+        return (TextView) findViewById(R.id.message);
+    }
+
+    private TextView getTextNumberTextView(){
+        return (TextView) findViewById(R.id.textnumber);
+    }
+
 
     //Sends an SMS message to another device
 
@@ -105,5 +130,22 @@ public class MainActivity extends AppCompatActivity {
                 messagewindow.show();
             }
         }
+    }
+
+    private Boolean checkIfReadyToSend(String phoneNumber, String body, String numberOfTexts) {
+        Boolean ready;
+        if (TextUtils.isEmpty(phoneNumber)){
+            ready=Boolean.FALSE;
+        }
+        else if (TextUtils.isEmpty(body)){
+            ready=Boolean.FALSE;
+        }
+        else if (TextUtils.isEmpty(numberOfTexts)){
+            ready=Boolean.FALSE;
+        }
+        else{
+            ready=Boolean.TRUE;
+        }
+        return ready;
     }
 }
